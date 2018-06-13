6b70aea7fea888ad1f7619a5c3c548f1d5bb8966
Raul Avila <raul@raulavila.com>
2018-06-13 15:43:48 +0100

    Add manual transactions example

--- spring-rest-api/src/main/kotlin/com/raulavila/springrestapi/person/PersonService.kt
+++ spring-rest-api/src/main/kotlin/com/raulavila/springrestapi/person/PersonService.kt
@@ -3,6 +3,13 @@ package com.raulavila.springrestapi.person
 import com.raulavila.springrestapi.audit.AuditService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+import javax.persistence.EntityManagerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.TransactionDefinition
+import org.springframework.transaction.support.DefaultTransactionDefinition
 
 @Service
 class PersonService(
@@ -24,9 +31,37 @@ class PersonService(
         return person
     }
 
-    fun updatePerson(person: Person): Person {
+    open fun updatePerson(person: Person): Person {
         val updatedPerson = personRepository.save(person)
         auditService.audit("Update person with ID: ${person.id}, changed NAME: ${person.name}")
         return updatedPerson
     }
 }
+
+//@Service
+//class TransactionalPersonService(
+//        personRepository: PersonRepository,
+//        auditService: AuditService
+//): PersonService(personRepository, auditService) {
+//
+//    @Autowired
+//    lateinit var transactionManager: PlatformTransactionManager
+//
+//    override fun updatePerson(person: Person): Person {
+//        println("-------- Updating person in transactional service")
+//
+//        val txDef = DefaultTransactionDefinition()
+//        val txStatus = transactionManager.getTransaction(txDef)
+//        try {
+//            val savedPerson = super.updatePerson(person)
+//
+//            transactionManager.commit(txStatus)
+//
+//            return savedPerson
+//        } catch (ex: Exception) {
+//            transactionManager.rollback(txStatus);
+//
+//            throw ex
+//        }
+//    }
+//}
